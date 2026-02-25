fa9b839c65c05911f03ee33ad276db8bc64741db
Anup Dhakal <dhakalanup015@gmail.com>
2026-02-25 11:07:15 +0545

    Group Anagrams

--- neetcode/group_anagram.py
+++ neetcode/group_anagram.py
@@ -9,7 +9,17 @@ class Solution:
             key = "".join(sorted(n))
             groups[key].append(n)
         return list(groups.values())
+    
+    def sortAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for n in strs:
+            count = [0] * 26
+            for c in n:
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(n)
+        return list(res.values())
 
 d = Solution()
 strs = ["act","pots","tops","cat","stop","hat"]
 print(d.groupAnagrams(strs))
+print(d.sortAnagrams(strs))
