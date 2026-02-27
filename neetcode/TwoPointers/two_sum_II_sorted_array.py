e2d0152ba3055b8d99eaf10e9ed84e28442469b5
Anup Dhakal <dhakalanup015@gmail.com>
2026-02-27 12:13:44 +0545

    Optimize solution

--- neetcode/TwoPointers/two_sum_II_sorted_array.py
+++ neetcode/TwoPointers/two_sum_II_sorted_array.py
@@ -23,10 +23,39 @@ class Solution:
                 return m 
         return -1 # not find the solution
 
+    def Binary2Sum(self, numbers: List[int], target: int) -> List[int]:
+        for i in range(len(numbers)):
+            temp = target - numbers[i]
+            l, r = i + 1, len(numbers) - 1
+            while l <= r:
+                mid = l + (r-l)//2
+                if numbers[mid] == temp:
+                    return [i+1, mid+1]
+                elif numbers[mid] < temp:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return []
+    
+    ## Two pointers
 
+    def twoSumPointer(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers) - 1
+        while l < r:
+            curtSum = numbers[l] + numbers[r]
+
+            if curtSum > target:
+                r -= 1
+            elif curtSum < target:
+                l += 1
+            else:
+                return [l + 1, r + 1]
+        return []
 
 
 numbers, target = [1,2,3,4], 3
 d = Solution()
 print(d.twoSum(numbers, target))
 print(d.search(numbers, target))
+print(d.Binary2Sum(numbers, target))
+print(d.twoSumPointer(numbers, target))
