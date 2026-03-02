011e7fd6575bd91a3218713bb1230e0acdace7b2
Anup Dhakal <dhakalanup015@gmail.com>
2026-03-02 10:44:52 +0545

    Remove Duplicates

---
+++ neetcode/TwoPointers/Leetcode/removeDuplicatesFromSortedArray.py
@@ -0,0 +1,7 @@
+from typing import List
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        pass
+d= Solution()
+nums = [0,0,1,1,1,2,2,3,3,4]
+print(d.removeDuplicates(nums))
