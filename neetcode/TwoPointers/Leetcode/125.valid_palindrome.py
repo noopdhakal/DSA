ee80443df37887dfc4fdcdf54b22743459e92851
Anup Dhakal <dhakalanup015@gmail.com>
2026-03-09 16:21:01 +0545

    isalnum() use

--- neetcode/TwoPointers/Leetcode/125.valid_palindrome.py
+++ neetcode/TwoPointers/Leetcode/125.valid_palindrome.py
@@ -2,9 +2,9 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         l, r = 0, len(s) - 1
         while l < r:
-            while l < r and not self.alphaNum(s[l]):
+            while l < r and not  s[l].isalnum():
                 l+=1
-            while r > l and not self.alphaNum(s[l]):
+            while r > l and not  s[r].isalnum():
                 r-=1
             if s[l].lower() != s[r].lower():
                 return False
@@ -17,8 +17,8 @@ class Solution:
                 ord('a') <= ord(c) <= ord('z') or
                 ord('0') <= ord(c) <= ord('9'))
     
-s = "Was it a car or a cat I saw?"
+s = "A man, a plan, a canal: Panama"
 
 d = Solution()
 print(d.isPalindrome(s))
-print(d.isPalindromeA(s))
+# print(d.isPalindromeA(s))
