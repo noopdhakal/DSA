898100516c78705aaebf1ee6047801b120d5d985
Anup Dhakal <dhakalanup015@gmail.com>
2026-03-10 22:30:34 +0545

    Reverse vowels of a String

---
+++ neetcode/TwoPointers/Leetcode/345.Reverse.py
@@ -0,0 +1,19 @@
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        vowels = "aeiouAEIOU"
+        s = list(s)
+        l, r = 0, len(s) - 1
+        while l < r: 
+            while l < r and s[l] not in vowels:
+                l += 1
+            while r > l and s[r] not in vowels:
+                r -= 1
+            
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
+        return "".join(s)
+
+s = "IceCreAm"
+d = Solution()
+print(d.reverseVowels(s))
