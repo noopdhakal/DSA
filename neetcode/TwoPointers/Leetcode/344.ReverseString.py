454945ca38e0dbac4dc504887a64604fa072d137
Anup Dhakal <dhakalanup015@gmail.com>
2026-03-10 22:35:27 +0545

    Reverse String

---
+++ neetcode/TwoPointers/Leetcode/344.ReverseString.py
@@ -0,0 +1,16 @@
+from typing import List
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        l, r = 0, len(s) - 1
+        while l < r: 
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
+        return s
+
+s = ["h","e","l","l","o"]
+d = Solution()
+print(d.reverseString(s))
