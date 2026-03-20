5b63b2ef266fde340bf3db2be2404992dad8ee10
Anup Dhakal <dhakalanup015@gmail.com>
2026-03-20 21:15:21 +0545

    Sort Array by Parity

---
+++ neetcode/TwoPointers/905.SortArrayByParity.py
@@ -0,0 +1,20 @@
+from typing import List
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        left, right = 0, len(nums) - 1
+    
+        while left < right:
+            if nums[left] % 2 > nums[right] % 2:
+                nums[left], nums[right] = nums[right], nums[left]
+            
+            if nums[left] % 2 == 0:
+                left += 1
+            if nums[right] % 2 == 1:
+                right -= 1
+                
+        return nums
+
+
+nums = [3,1,2,4]
+d = Solution()
+print(d.sortArrayByParity(nums))
