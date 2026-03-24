4b6b503f205d181f82db7ee76d965061fa992b85
Anup Dhakal <dhakalanup015@gmail.com>
2026-03-24 09:10:21 +0545

    541 String Reversal II

---
+++ neetcode/TwoPointers/Leetcode/541.StringReversal.py
@@ -0,0 +1,22 @@
+from typing import List
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        s = list(s) # immutable 
+        n = len(s)
+        
+        for start in range(0, n, 2*k):
+
+            left = start
+            right = min(start + k - 1, n - 1)
+            while left < right:
+                s[left], s[right] = s[right], s[left]
+                left += 1
+                right -= 1
+        return "".join(s)
+        
+
+
+s = "abcdefg"
+k = 2
+d = Solution()
+print(d.reverseStr(s, k))
